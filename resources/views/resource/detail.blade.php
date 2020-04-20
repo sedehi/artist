e38e302f97c0f757a4fd0a768323790425de807f
Navid Sedehi <navid.sedehi@gmail.com>
2020-04-21 03:36:07 +0430

    edit btn in detail

--- resources/views/resource/detail.blade.php
+++ resources/views/resource/detail.blade.php
@@ -2,7 +2,16 @@
 @section('content')
     <div class="col-lg-12">
         <div class="card">
-            <div class="card-header">Simple Table</div>
+            <div class="card-header">
+                <div class="row">
+                    <div class="col-md-3 offset-md-8">
+                        <h4>header</h4>
+                    </div>
+                    <div class="col-md-1">
+                        <a href="{{action([\Sedehi\Artist\Http\Controllers\EditController::class,'edit'],['section' => $section,'resource' => $resourceName,$resourceId => $resourceId])}}" class="btn btn-md btn-info text-white">Edit</a>
+                    </div>
+                </div>
+            </div>
             <div class="card-body">
                 <table class="table table-responsive-sm table-bordered">
                         @foreach($resource->fieldsForDetail() as $field)
--- src/Http/Controllers/DetailController.php
+++ src/Http/Controllers/DetailController.php
@@ -6,23 +6,23 @@ use Illuminate\Routing\Controller as BaseController;
 
 class DetailController extends BaseController
 {
-    public function __invoke($section, $resource, $resourceId)
+    public function __invoke($section, $resourceName, $resourceId)
     {
-        if ($section == null && $resource == null) {
+        if ($section == null && $resourceName == null) {
             abort(404);
         }
 
-        if ($resource == null) {
-            $resource = $section;
+        if ($resourceName == null) {
+            $resourceName = $section;
             $section = null;
         }
 
-        $resourceFile = app()->getNamespace().config('artist.resource_path').'\\'.$resource;
+        $resourceFile = app()->getNamespace().config('artist.resource_path').'\\'.$resourceName;
 
         $resource = new $resourceFile;
         $query = $resource::$model::query();
         $item = $query->findOrFail($resourceId);
 
-        return view($resource::$detailView, compact('section', 'resource', 'item'));
+        return view($resource::$detailView, compact('section', 'resource', 'item','resourceName','resourceId'));
     }
 }
