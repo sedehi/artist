72123e079b06fbcee430a0cd5f3e316411114cd0
Navid Sedehi <navid.sedehi@gmail.com>
2020-04-26 02:04:43 +0430

    wip

---
+++ src/Http/Requests/CreateRequest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Sedehi\Artist\Http\Requests;
+
+use Sedehi\Artist\Traits\Resource;
+
+class CreateRequest extends ArtistRequest
+{
+    use Resource;
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        $resourceClass = $this->getResource();
+
+        return $this->getRules(
+            (new $resourceClass)->fieldsForCreate()
+        );
+    }
+
+
+
+}
