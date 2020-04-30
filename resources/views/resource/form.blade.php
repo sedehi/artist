afe7e39d549578906b4a2e9a53deb066dc05226e
Majid Ghaemifar <mgh145@gmail.com>
2020-05-01 02:01:25 +0430

    add hidden field

--- resources/views/resource/form.blade.php
+++ resources/views/resource/form.blade.php
@@ -11,14 +11,18 @@
     @csrf
     <div class="card-body">
         @foreach($fields as $field)
-            <div class="form-group row mb-4">
-               {!! $field->model($item ?? null)->render() !!}
-            </div>
+            @if ($field->type == 'hidden')
+                {!! $field->model($item ?? null)->render() !!}
+            @else
+                <div class="form-group row mb-4">
+                    {!! $field->model($item ?? null)->render() !!}
+                </div>
+            @endif
         @endforeach
-</div>
-<div class="card-footer">
-    <button class="btn btn-md btn-primary" type="submit">Create</button>
-    <button class="btn btn-md btn-primary" type="submit">Create and Add Another</button>
-    <button class="btn btn-md btn-danger" type="reset"> Reset</button>
-</div>
+    </div>
+    <div class="card-footer">
+        <button class="btn btn-md btn-primary" type="submit">Create</button>
+        <button class="btn btn-md btn-primary" type="submit">Create and Add Another</button>
+        <button class="btn btn-md btn-danger" type="reset"> Reset</button>
+    </div>
 </form>
