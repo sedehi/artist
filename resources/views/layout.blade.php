3995d1c5228adc5b3c333da172cb6b2951570d63
Navid Sedehi <navid.sedehi@gmail.com>
2020-08-28 20:46:15 +0430

    add template

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,39 +1,57 @@
-<!doctype html>
-<html lang="{{app()->getLocale()}}" dir="{{config('artist.dir')}}">
+<!DOCTYPE html>
+@if(in_array(app()->getLocale(),['fa','ar']))
+    @php($direction = 'rtl')
+@else
+    @php($direction = 'ltr')
+@endif
+<html lang="{{app()->getLocale()}}"  dir="{{$direction}}">
 <head>
-    <meta charset="utf-8"/>
-    <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover"/>
-    <meta http-equiv="X-UA-Compatible" content="ie=edge"/>
-    <title>Admin</title>
-    <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin>
-    <meta name="msapplication-TileColor" content="#206bc4"/>
-    <meta name="theme-color" content="#206bc4"/>
-    <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent"/>
-    <meta name="apple-mobile-web-app-capable" content="yes"/>
-    <meta name="mobile-web-app-capable" content="yes"/>
-    <meta name="HandheldFriendly" content="True"/>
-    <meta name="MobileOptimized" content="320"/>
-    <meta name="robots" content="noindex,nofollow,noarchive"/>
-    <link href="{{asset('admin/css/tabler.min.css')}}" rel="stylesheet"/>
-    <link href="{{asset('admin/css/all.min.css')}}" rel="stylesheet"/>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>@yield('title')</title>
+
+    <link href="https://cdn.materialdesignicons.com/4.4.95/css/materialdesignicons.min.css" rel="stylesheet" />
+
+    @if($direction == 'rtl')
+        <link rel="stylesheet" href="{{asset('admin/css/sleek.min.rtl.css')}}" />
+    @else
+        <link rel="stylesheet" href="{{asset('admin/css/sleek.min.css')}}" />
+    @endif
+    <!--
+      HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries
+    -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
     @stack('css')
 </head>
-<body class="antialiased">
-@include('artist::header')
-<div class="page">
-    @include('artist::header-responsive')
-    <div class="content">
-        <div class="container-fluid">
-            <div class="row">
-             @yield('content')
+
+
+<body class="header-static sidebar-fixed sidebar-dark header-dark" id="body">
+
+
+
+<div class="wrapper">
+
+    @include('artist::sidebar')
+
+    <div class="page-wrapper">
+        <!-- Header -->
+        @include('artist::header')
+
+        <div class="content-wrapper">
+            <div class="content">
+                @yield('content')
             </div>
         </div>
     </div>
 </div>
-<!-- Libs JS -->
-<script src="{{asset('admin/libs/bootstrap/dist/js/bootstrap.bundle.min.js')}}"></script>
-<!-- Tabler Core -->
-<script src="{{asset('admin/js/tabler.min.js')}}"></script>
+<script src="{{asset('admin/plugins/jquery/jquery.min.js')}}"></script>
+<script src="{{asset('admin/plugins/slimscrollbar/jquery.slimscroll.min.js')}}"></script>
+<script src="{{asset('admin/js/sleek.bundle.js')}}"></script>
 @stack('js')
 </body>
 </html>
--- resources/views/resource/detail.blade.php
+++ resources/views/resource/detail.blade.php
@@ -1,14 +1,13 @@
 @extends('artist::layout')
 @section('content')
-    <div class="col-lg-12">
-        <div class="card">
-            <div class="card-header">
-                <div class="row">
-                    <div class="col-md-3 offset-md-8">
-                        <h4>header</h4>
+        <div class="card card-default">
+            <div class="card-header card-header-border-bottom">
+                    <div class="col-md-10">
+                        <h4 class="w-100 p-3">header</h4>
                     </div>
-                    <div class="col-md-1">
-                        <a href="{{route('artist.resource.edit',['section' => $section,'resource' => $resourceName,$resourceId => $resourceId])}}" class="btn btn-md btn-info text-white">Edit</a>
+                    <div class="col-md-2">
+                        <a href="{{route('artist.resource.edit',['section' => $section,'resource' => $resourceName,$resourceId => $resourceId])}}" class="btn btn-md btn-info text-white float-left mr-3">Edit</a>
+
                         <form method="post" action="{{route('artist.resource.destroy',['section' => $section,'resource' => $resourceName])}}">
                             @method('delete')
                             @csrf
@@ -16,15 +15,13 @@
                             <button class="btn btn-md btn-danger text-white">Delete</button>
                         </form>
                     </div>
-                </div>
             </div>
-            <div class="card-body">
-                <table class="table table-responsive-sm table-bordered">
+            <div class="card-body p-4">
+                <table class="table table-striped">
                     @foreach($fields as $field)
                         <td>{{ $field->model($item)->displayValue() }}</td>
                     @endforeach
                 </table>
             </div>
         </div>
-    </div>
 @endsection
