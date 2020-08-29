488ba88511a0cfb76b8a54aec100849c978d105f
Majid Ghaemifar <mgh145@gmail.com>
2020-08-29 21:45:25 +0430

    wip password field

---
+++ src/Fields/Password.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Sedehi\Artist\Fields;
+
+class Password extends Field
+{
+    protected $viewPath = 'artist::fields.password';
+    public $type = 'password';
+
+    protected $showOnIndex = false;
+    protected $showOnDetail = false;
+    protected $showOnCreate = true;
+    protected $showOnUpdate = true;
+
+    public function __construct()
+    {
+        $this->name = 'password';
+
+        parent::name($this->name);
+
+        parent::__construct();
+    }
+
+    public static function make()
+    {
+        return new self;
+    }
+}
