8d6021d2c7debb2287eb7a024946ac843fec69d6
Navid Sedehi <navid.sedehi@gmail.com>
2020-08-29 13:51:37 +0430

    fix detail page bugs

--- resources/views/resource/detail.blade.php
+++ resources/views/resource/detail.blade.php
@@ -6,14 +6,14 @@
                         <h4 class="w-100 p-3">header</h4>
                     </div>
                     <div class="col-md-3">
-                        <a href="{{route('artist.resource.edit',['section' => $section,'resource' => $resourceName,'resourceId' => $resourceId])}}" class="btn btn-md btn-info text-white float-right ml-3">Edit</a>
+                        <a href="{{route('artist.resource.edit',['section' => $section,'resource' => $resourceName,'resourceId' => $resourceId])}}" class="btn btn-md btn-info text-white float-right ml-2">Edit</a>
                         <form class="float-right" method="post" action="{{route('artist.resource.destroy',['section' => $section,'resource' => $resourceName])}}">
                             @method('delete')
                             @csrf
                             <input type="hidden" name="id[]" value="{{ $resourceId }}">
                             <button class="btn btn-md btn-danger text-white">Delete</button>
                         </form>
-                        <a href="{{route('artist.resource.index',['section' => $section,'resource' => $resourceName])}}" class="btn btn-md btn-primary text-white float-right mr-3">List</a>
+                        <a href="{{route('artist.resource.index',['section' => $section,'resource' => $resourceName])}}" class="btn btn-md btn-primary text-white float-right mr-2">List</a>
                     </div>
             </div>
             <div class="card-body p-4">
