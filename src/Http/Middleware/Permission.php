b0b41f97a08efae2f3ac5430f91e4faaf78557c3
Navid Sedehi <navid.sedehi@gmail.com>
2021-05-29 20:42:28 +0430

    middlewares

---
+++ src/Http/Middleware/Permission.php
@@ -0,0 +1,45 @@
+<?php
+
+
+namespace Sedehi\Artist\Http\Middleware;
+
+
+use Closure;
+use Illuminate\Contracts\Auth\Guard;
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Route;
+
+class Permission
+{
+
+    private $allowed = [];
+
+    public function __construct(Guard $auth)
+    {
+        $this->auth = $auth;
+    }
+
+    public function handle($request, Closure $next)
+    {
+        if (in_array(Route::currentRouteAction(), $this->allowed)) {
+            return $next($request);
+        }
+
+        $currentController = explode('\\', strtolower(Route::currentRouteAction()));
+
+        $sectionName = $currentController[3];
+        $method      = explode('@', end($currentController));
+        $controller  = reset($method);
+        $method      = end($method);
+
+        if (Gate::allows($sectionName.'.'.$controller.'.'.$method)) {
+            return $next($request);
+        }
+
+        if ($request->expectsJson()) {
+            return response('Unauthorized.', 403);
+        } else {
+            return redirect()->route('artist.home')->with('error', trans('auth.access_denied'));
+        }
+    }
+}
