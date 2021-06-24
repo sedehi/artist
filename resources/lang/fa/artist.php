5a2dd27baca61ccf77931bc101a0a82aca2537ba
Majid Ghaemifar <mgh145@gmail.com>
2021-06-25 00:21:35 +0430

    add messages to artist translations

--- resources/lang/fa/artist.php
+++ resources/lang/fa/artist.php
@@ -3,6 +3,8 @@
 return [
     'name' => 'مدیریت',
     'select_an_option'  =>  'انتخاب کنید',
+    'select_this_part'  =>  'انتخاب این قسمت',
+    'select_all'  =>  'انتخاب همه',
     'titles' => [
         'home' => 'مدیریت',
     ],
@@ -17,10 +19,15 @@ return [
     'delete' => 'حذف',
     'submit' => 'ذخیره',
     'back' => 'بازگشت',
-    'saved' => 'ذخیره شد',
-    'deleted' => 'حذف شد',
     'management_group' => 'گروه مدیریتی',
     'search' => 'جستجو',
     'all_data' => 'تمام اطلاعات',
     'no_data_to_show' => 'اطلاعاتی برای نمایش وجود ندارد.',
+    'modification_only_by_owner' => 'تغییر فقط توسط کاربر ایجاد کننده',
+
+    'messages' => [
+        'saved' => 'اطلاعات با موفقیت ذخیره شد',
+        'updated' => 'اطلاعات با موفقیت بروز رسانی شد',
+        'deleted' => 'اطلاعات با موفقیت حذف شد',
+    ],
 ];
