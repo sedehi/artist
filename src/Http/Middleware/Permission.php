e0a1905a97b6eff06875c555bcc574c5a9c75410
Navid Sedehi <navid.sedehi@gmail.com>
2021-10-24 19:19:54 +0330

    custom permission

--- src/Http/Middleware/Permission.php
+++ src/Http/Middleware/Permission.php
@@ -25,6 +25,7 @@ class Permission
     public function __construct(Guard $auth)
     {
         $this->auth = $auth;
+        $this->allowed = array_merge($this->allowed,config('artist.allow_permissions',[]));
     }
 
     public function handle($request, Closure $next)
